5bc3defe0432e60a13f57f294746aa2832d15fc5
scattter <413365742@qq.com>
2020-09-17 21:33:01 +0800

    [zk] finsh Practice-1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.ZoneOffset;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -10,7 +11,16 @@ import java.time.LocalDate;
  */
 public class Practice1 {
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+        int targeMonth = 5;
+        int targetDay = 1;
+        int timeStampToDayUnit = 1000 * 3600 * 24;
+        int targetYear = date.getMonthValue() > targeMonth - 1 ? date.getYear() + 1 : date.getYear();
+        LocalDate targetDate = LocalDate.of(targetYear, targeMonth, targetDay);
+        long dateTimeStamp = date.atStartOfDay(ZoneOffset.ofHours(8)).toInstant().toEpochMilli();
+        long targetTimeStamp = targetDate.atStartOfDay(ZoneOffset.ofHours(8)).toInstant().toEpochMilli();
+        long day = (targetTimeStamp - dateTimeStamp) / timeStampToDayUnit;
+
+        return day;
+    }
 }
