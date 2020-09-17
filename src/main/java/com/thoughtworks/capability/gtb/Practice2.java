ebf3d5076f5c37413924947b079f87899c6562be
scattter <413365742@qq.com>
2020-09-17 21:58:00 +0800

    [zk] finsh Practice-2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+
+import static java.time.DayOfWeek.*;
 
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
@@ -10,7 +13,10 @@ import java.time.LocalDate;
  */
 public class Practice2 {
 
-  public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
-  }
+    public static LocalDate getNextWorkDate(LocalDate date) {
+        int dateCompareWithFriday = 0;
+        int oneDay = 1;
+        DayOfWeek weekDay = date.getDayOfWeek();
+        return weekDay.compareTo(FRIDAY) < dateCompareWithFriday ? date.plusDays(FRIDAY.compareTo(weekDay)) : date.plusDays((SUNDAY.compareTo(weekDay) + oneDay));
+    }
 }
